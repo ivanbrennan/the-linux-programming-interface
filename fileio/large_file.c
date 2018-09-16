5a299d317a31ced0c83775e3eb9c98e9120c1c0f
ivanbrennan <ivan.brennan@gmail.com>
2018-09-16 10:36:11 -0400

    -D_FILE_OFFSET_BITS=64

--- fileio/large_file.c
+++ fileio/large_file.c
@@ -12,13 +12,13 @@ main(int argc, char *argv[])
 	if (argc != 3 || strcmp(argv[1], "--help") == 0)
 		usageErr("%s pathname offset\n", argv[0]);
 
-	fd = open64(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	fd = open(argv[1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-		errExit("open64");
+		errExit("open");
 
 	off = atoll(argv[2]);
-	if (lseek64(fd, off, SEEK_SET) == -1)
-		errExit("lseek64");
+	if (lseek(fd, off, SEEK_SET) == -1)
+		errExit("lseek");
 
 	if (write(fd, "test", 4) == -1)
 		errExit("write");
