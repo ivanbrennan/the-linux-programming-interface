a5f667db3fa3f6b8868300a48563d9eb889961a2
ivanbrennan <ivan.brennan@gmail.com>
2018-09-26 08:29:35 -0400

    dup

---
+++ fileio/dup.c
@@ -0,0 +1,28 @@
+#include "tlpi_hdr.h"
+
+int
+main(int argc, char *argv[])
+{
+	int oldfd, newfd, fd;
+	ssize_t nread;
+	char buf[1];
+
+	oldfd = 0;
+	newfd = dup(oldfd);
+
+	fd = oldfd;
+	while (1) {
+		nread = read(fd, &buf, 1);
+
+		if (nread == -1) {
+			errExit("read");
+		} else if (nread == 0) {
+			break;
+		} else {
+			printf("fd: %d, buf: %c\n", fd, buf[0]);
+			fd = (fd == oldfd) ? newfd : oldfd;
+		}
+	}
+
+	exit(EXIT_SUCCESS);
+}
