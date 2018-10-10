971b2c49c1e7d00f48d1437b817f3796640a6d36
ivanbrennan <ivan.brennan@gmail.com>
2018-10-10 07:54:44 -0400

    free_and_sbrk: print program break after malloc

--- memalloc/free_and_sbrk.c
+++ memalloc/free_and_sbrk.c
@@ -34,6 +34,7 @@ main(int argc, char *argv[])
 		ptr[j] = malloc(blockSize);
 		if (ptr[j] == NULL)
 			errExit("malloc");
+		printf("After malloc(), program break is: %10p\n", sbrk(0));
 	}
 
 	printf("Program break is now:	%10p\n", sbrk(0));
